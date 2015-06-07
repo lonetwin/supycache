lonetwin/supycache

commit 27e2a6743240a8e320dbc7f2b8ae48216324c850

Better docs and less restriction on backend implemetation
Wrote some doc strings and removed the requirement that the `backend` have a
`connect()` method.

supycache/__init__.py
@@ -1,20 +1,61 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+supycache - Simple yet capable caching decorator for python.
 
-default_backend = None
+https://github.com/lonetwin/supycache
+https://supycache.readthedocs.org/en/latest/
+"""
 
-def get_default_backend():
-    return DictCache if default_backend is None else default_backend
+__author__ = "Steven Fernandez <steve@lonetwin.net>"
+__license__ = "MIT"
+
+from functools import wraps
 
 from .backends import DictCache
 from .cdf import CacheDecoratorFactory
 
+default_backend = None
+
+def get_default_backend():
+    """Returns the currently configured `default_backend`.
+
+    The ``default_backend`` is a ``supycache.DictCache`` instance if not set.
+    Use ``supycache.set_default_backend`` to change this. A ``backend`` is any
+    (caching) object that has at least the `.get()``, ``.set()`` and
+    ``.delete()`` methods.
+    """
+    return default_backend if default_backend else DictCache()
+
 def supycache(**options):
+    """Decorates a function for caching/expiring cache depending on arguments.
+
+    This is the primary interface to use `supycache`. This decorator accepts
+    the following parameters:
+
+    - ``cache_key`` : Either a simple string, a format string or callable used
+                      to create the key used for caching the result of the
+                      function being decorated.
+                      This key will be resolved at run-time and would be
+                      evaluated against/with the parameters passed to the
+                      function being decorated.
+
+    - ``expire_key`` : Either a simple string, a format string or callable used
+                       to create the key that would be expired before the
+                       decorated function is called.
+                       This key will be resolved at run-time and would be
+                       evaluated against/with the parameters pass to the
+                       function being decorated.
+
+    - `ignore_errors` : A boolean to indicate whether errors in getting,
+                        setting or expiring cache should be ignored or
+                        re-raised on being caught.
+    """
     def prepare_inner(function):
         recognized_options = {'cache_key',
                               'expire_key',
-                              'key_creator',
-                              }
+                              'ignore_errors',
+                             }
 
         if recognized_options.isdisjoint(options):
             raise KeyError('expecting one of %s as an argument' % \

supycache/cdf.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
-from . import get_default_backend
-
 class CacheDecoratorFactory:
     def __init__(self, backend, cache_key='', expire_key='', **other_kwargs):
         self._backend = backend
@@ -22,19 +20,12 @@ class CacheDecoratorFactory:
     def __call__(self, func):
         return self._wrapped(func)
 
-    @property
-    def backend(self):
-        if not self._backend:
-            self._backend = get_default_backend()
-            self._backend.connect(**self._options)
-        return self._backend
-
     def _expiry_wrapper(self, func):
         @wraps(func)
         def cache_deleter(*args, **kwargs):
-            key = self.key.format(*args, **kwargs)
+            key = self.key(*args, **kwargs) if callable(self.key) else self.key.format(*args, **kwargs)
             try:
-                self.backend.delete(key)
+                self._backend.delete(key)
             except:
                 if not self.ignore_errors:
                     raise
@@ -46,9 +37,9 @@ class CacheDecoratorFactory:
         @wraps(func)
         def cache_setter(*args, **kwargs):
             result = None
-            key = self.key.format(*args, **kwargs)
+            key = self.key(*args, **kwargs) if callable(self.key) else self.key.format(*args, **kwargs)
             try:
-                result = self.backend.get(key)
+                result = self._backend.get(key)
             except:
                 if not self.ignore_errors:
                     raise
@@ -56,7 +47,7 @@ class CacheDecoratorFactory:
             if not result:
                 result = func(*args, **kwargs)
                 try:
-                    self.backend.set(key, result)
+                    self._backend.set(key, result)
                 except:
                     if not self.ignore_errors:
                         raise

tests/supycache_tests.py
@@ -53,19 +53,6 @@ class TestCacheDecorators(unittest.TestCase):
 
         self.assertTrue('expecting one of' in context.exception.message)
 
-    def test_unconnected_backend(self):
-        """ unconnected backend
-        """
-        from supycache.cdf import CacheDecoratorFactory
-        supycache.default_backend = None # override setUp()
-        cdf = CacheDecoratorFactory(backend=None, cache_key='simple_key',
-                ignore_errors=False)
-
-        with self.assertRaises(AttributeError) as context:
-            cdf(lambda _: None)('dummy')
-
-        self.assertTrue("'NoneType' object has no attribute 'connect'" in
-                context.exception.message)
 
     def test_do_not_ignore_errors(self):
         """ do not ignore errors

commit 9713b521ff03dffa34dedd84122486f9e25f7a47

Fix a regression (introduced in 27e2a67) while setting default backend

supycache/__init__.py
@@ -25,7 +25,10 @@ def get_default_backend():
     (caching) object that has at least the `.get()``, ``.set()`` and
     ``.delete()`` methods.
     """
-    return default_backend if default_backend else DictCache()
+    global default_backend
+    if not default_backend:
+        default_backend = DictCache()
+    return default_backend
 
 def supycache(**options):
     """Decorates a function for caching/expiring cache depending on arguments.
@@ -61,6 +64,6 @@ def supycache(**options):
             raise KeyError('expecting one of %s as an argument' % \
                 ','.join(recognized_options))
 
-        cdf = CacheDecoratorFactory(default_backend, **options)
+        cdf = CacheDecoratorFactory(options.get('backend', get_default_backend()), **options)
         return cdf(function)
     return prepare_inner